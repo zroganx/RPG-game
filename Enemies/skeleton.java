9eef4cd5268135ab14b7c2a996f2389ff7ae4170
zrogan <zrogan@arogan.com>
2020-11-23 19:59:24 +0000

    remove @overides

--- Enemies/skeleton.java
+++ Enemies/skeleton.java
@@ -10,42 +10,34 @@ public class skeleton extends EnemyBasicAI implements Enemies
     {
         return enemyTotalHealth;
     }
-    @Override
     public String getEnemyName()
     {
         return enemyName;
     }
-    @Override
     public boolean getEnemyEscape()
     {
         return enemyEscape;
     }
-    @Override
     public boolean getEnemyBlocking()
     {
         return enemyBlocking;
     }
-    @Override
     public int getEnemyHealth()
     {
         return enemyHealth;
     }
-    @Override
     public int getEnemyAtt()
     {
         return enemyAtt;
     }
-    @Override
     public void setEnemyBlocking(boolean s)
     {
         enemyBlocking = s;
     }
-    @Override
     public void setEnemyHealth(int s)
     {
         enemyHealth += s;
     }
-    @Override
     public void setEnemyEscape(boolean s)
     {
         enemyEscape = s;
